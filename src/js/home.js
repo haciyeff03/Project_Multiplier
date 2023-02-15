90a0e679de97e87b8dd16591f3c23e8e819fbea2
Your Name <haciyevqurban97@gmail,com>
2023-02-15 21:43:20 +0400

    home section is ready

--- src/js/home.js
+++ src/js/home.js
@@ -1,16 +1,14 @@
-const Tab = document.querySelectorAll(".tab");
-const Tabcont = document.querySelectorAll(".tab-cont");
-
-for (let j = 0; j < Tab.length; j++) {
-  Tab[j].addEventListener("click", () => {
-    console.log("salam");
-    for (let k = 0; k < Tab.length; k++) {
-      if (k != j && Tab[k].classList.contains("active-tab")) {
-        Tab[k].classList.remove("active-tab");
-        Tabcont[k].classList.remove("active-tabcont");
-      }
-    }
-    Tab[j].classList.add("active-tab");
-    Tabcont[j].classList.add("active-tabcont");
-  });
-}
+var swiper = new Swiper(".mySwiper3", {
+  slidesPerView: "auto",
+  spaceBetween: 40,
+  grabCursor: true,
+  loop: true,
+  breakpoints: {
+    768: {
+      spaceBetween: 90,
+    },
+    300: {
+      spaceBetween: 30,
+    },
+  },
+});
